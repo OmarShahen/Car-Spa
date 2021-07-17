a386995983a52c2d6504f2290039800b742bf4a6
Omar Reda <omarredaelsayedmohamed@gmail.com>
2021-07-16 19:58:46 -0700

    Changed configration file for the database

--- config/config.js
+++ config/config.js
@@ -1,12 +1,18 @@
 
 module.exports = {
     db:{
-        user: 'postgres',
+        /*user: 'postgres',
         host: 'localhost',
         database: 'car-spa',
         password: 'Shahen77',
         port: 5432,
-        ssl: true
+        ssl: true*/
+        user: 'doadmin',
+        host: 'db-postgresql-nyc3-74306-do-user-9140176-0.b.db.ondigitalocean.com',
+        database: 'defaultdb',
+        password: 'nfqxwylnqbmk97fm',
+        port: '25060',
+        sslmode: 'require'
     },
     secretKey: 'obgpoirhgi0h3-wojvo3jgb=034jv=-0o43nb-0gng',
     mailAccount: 'autocarspa77@gmail.com',
