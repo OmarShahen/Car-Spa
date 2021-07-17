4c861e7c5ca714999da2a31ffe0d0419bac5abd7
Omar Reda <omarredaelsayedmohamed@gmail.com>
2021-07-17 01:55:50 -0700

    Changed Configuration

--- config/config.js
+++ config/config.js
@@ -7,12 +7,17 @@ module.exports = {
         password: 'Shahen77',
         port: 5432,
         ssl: true*/
-        user: 'doadmin',
+        /*user: 'doadmin',
         host: 'db-postgresql-nyc3-74306-do-user-9140176-0.b.db.ondigitalocean.com',
         database: 'car-spa',
         password: 'nfqxwylnqbmk97fm',
         port: '25060',
-        sslmode: 'require'
+        sslmode: 'require'*/
+        user: 'omar',
+        host: 'localhost',
+        database: 'carspa',
+        password: 'Shahen77',
+        port: 5432
     },
     secretKey: 'obgpoirhgi0h3-wojvo3jgb=034jv=-0o43nb-0gng',
     mailAccount: 'autocarspa77@gmail.com',
