aa20db77c75d5474b6b470da30aee4947131f47a
Omar Reda <omarredaelsayedmohamed@gmail.com>
2021-12-17 19:41:02 -0800

    added keys file

---
+++ config/keys.js
@@ -0,0 +1,26 @@
+module.exports = {
+    db:{
+        user: 'reda',
+        host: 'localhost',
+        database: 'carspa',
+        password: 'reda77',
+        port: 5432
+    },
+    port: 5000,
+    customerSecretKey: 'obgpoirhgi0h3-wojvo3jgb=034jv=-0o43nb-0gng',
+    employeeSecretKey: 'obih-4g-4npgneongenvefojgldjvoejogjgjrljlr',
+    adminSecretKey: 'ubf98fwnksnnbp3p3pvnpnpfneih40g-4493ncwepvnpg',
+    bcryptRounds: 8,
+    mailAccount: 'autocarspa77@gmail.com',
+    mailAccountPassword: 'Nashar77',
+    employeesFiles: '../',
+    twilio:{
+        accountsid: 'AC85f4e1417efbe3aeaa90f3f8a066dd0a',
+        authToken: 'add73916c99ed9e7ff1b41343ad1be26'
+    },
+
+    smsAPI:{
+        apiKey: '5gPRNBgjZBVoPiPtikSgbejd3',
+        originator: 'CAR-SPA'
+    }
+}
